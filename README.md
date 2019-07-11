4063d5d07b1a82a5eeae1224ca3ab7db94caee48
Andrei I <contact@cloudemoz.com>
2019-07-11 16:37:29 -0400

    v2 accounted for edge cases

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # gcp-assets
 parse gcloud assets export output and save to BQ usingg Dataflow GCS to BQ template
+
+this code parses the output of the following command
+gcloud asset export --output-path "gs://$BUCKET/assets.json" --content-type=resource --organization=$ORG_NUMBER
+https://cloud.google.com/sdk/gcloud/reference/asset/export
+
+and extracts VM names and OS name
+
+focuses on some edge cases, tested with a large org (thousands of vms)
+
+found this after the fact
+https://github.com/GoogleCloudPlatform/professional-services/tree/master/tools/asset-inventory
+
+then simply do:
+
+SELECT vm_license_name, count(*) FROM `PROJECT.DATASET.vms2` 
+where vm_license_name is not null 
+group by vm_license_name 
+order by 2 desc
--- asset-transform.js
+++ asset-transform.js
@@ -6,14 +6,30 @@ function transformx(line) {
     json_out.name = json_in.name;
     json_out.asset_type = json_in.asset_type;
 
-    if (json_in.asset_type == "compute.googleapis.com/Instance"){
+    if (json_in.asset_type == "compute.googleapis.com/Instance") {
 
         var name_array = json_in.name.split("/");
         json_out.vm_name = name_array[name_array.length - 1];
         json_out.vm_project = name_array[name_array.length - 5];
 
-        var name_array = json_in.resource.data.disk[0].license[0].split("/");
-        json_out.vm_license_name = name_array[name_array.length - 1];
+        var data = json_in.resource.data;
+        if (data.hasOwnProperty("disk") && Array.isArray(data.disk)) {
+
+            var disk0 = json_in.resource.data.disk[0];
+            //console.log("NOT edge case!");
+
+            if (disk0.hasOwnProperty("license")) {
+                //console.log("yes");
+                var name_array = disk0.license[0].split("/");
+                json_out.vm_license_name = name_array[name_array.length - 1];
+            } else {
+                //console.log("no");
+                json_out.source_disk = disk0.source
+            }
+        } else {
+            //console.log("edge case!");
+            json_out.edge_case = JSON.stringify(data);
+        }
 
         var jsonString = JSON.stringify(json_out);
 
